687fa0c0ef2a976a527668f9290d63601414c68d
endblack <75638685+endblack@users.noreply.github.com>
2021-01-19 14:15:21 -0300

    Create bruxinhomenu.js

---
+++ src/bruxinhomenu.js
@@ -0,0 +1,12 @@
+const bruxinhomenu = (prefix, pushname) => {
+    return `◪ *BOT_MEGAH*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.bruxinhomenu = bruxinho
